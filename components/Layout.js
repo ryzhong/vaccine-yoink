6597508cc0bde729503662a1c187b051ae3d1da2
ryzhong <ryzhong92@gmail.com>
2021-04-21 23:46:41 -0700

    Created Layout Created Info Page

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+export default props => (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+        <main>
+            {props.content}
+        </main>
+    </div>
+)
